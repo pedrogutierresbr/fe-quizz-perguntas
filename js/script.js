835d1a13ba36ee419b3eefbe3f52cb7ad8ad816f
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-10-17 17:16:04 -0300

    Shown question alternatives

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function init() {
     createQuestion(0);
 }
 
-// criar uma pergunta (limpar questão anterior e alterar texto pergunta)
+// criar uma pergunta (limpar questão anterior, alterar texto pergunta e inserir alternativas)
 function createQuestion(i) {
     const oldButtons = answersBox.querySelectorAll("button");
     oldButtons.forEach(function (btn) {
@@ -26,7 +26,30 @@ function createQuestion(i) {
     const questionNumber = question.querySelector("#question-number");
 
     questionNumber.textContent = i + 1;
-    questionText.textContent = questions[0].question;
+    questionText.textContent = questions[i].question;
+
+    questions[i].answers.forEach((answer, index) => {
+        const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
+
+        const letterBtn = answerTemplate.querySelector(".btn-letter");
+        const answerText = answerTemplate.querySelector(".question-answer");
+
+        letterBtn.textContent = letters[index];
+        answerText.textContent = answer["answer"];
+
+        answerTemplate.setAttribute("correct-answer", answer["correct"]);
+
+        answerTemplate.classList.remove("hide");
+        answerTemplate.classList.remove("answer-template");
+
+        answersBox.appendChild(answerTemplate);
+
+        answerTemplate.addEventListener("click", () => {
+            console.log(index);
+        });
+    });
+
+    actualQuestions++;
 }
 
 init();
