de8ecd22537312b8aad924bb2202cfe46ca561ed
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-10-17 16:08:26 -0300

    Defined initial variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("hey man");
+//Variáveis
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestions = 0;
