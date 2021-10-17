8db7f49e439d67c6c9f438957113e0c5310ad2ca
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-10-17 16:53:10 -0300

    Created question module and init function

---
+++ js/questions.js
@@ -0,0 +1,65 @@
+export default [
+    {
+        question: "PHP foi desenvolvido para qual fim?",
+        answers: [
+            {
+                answer: "back-end",
+                correct: true,
+            },
+            {
+                answer: "front-end",
+                correct: false,
+            },
+            {
+                answer: "Sistema operacional",
+                correct: false,
+            },
+            {
+                answer: "Banco de dados",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "Uma forma de declarar variável em JavaScript:",
+        answers: [
+            {
+                answer: "$var",
+                correct: false,
+            },
+            {
+                answer: "var",
+                correct: true,
+            },
+            {
+                answer: "@var",
+                correct: false,
+            },
+            {
+                answer: "#let",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "Qual o seletor de id no CSS?",
+        answers: [
+            {
+                answer: "#",
+                correct: true,
+            },
+            {
+                answer: ".",
+                correct: false,
+            },
+            {
+                answer: "@",
+                correct: false,
+            },
+            {
+                answer: "/",
+                correct: false,
+            },
+        ],
+    },
+];
--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ const scoreContainer = document.querySelector("#score-container");
 const letters = ["a", "b", "c", "d"];
 let points = 0;
 let actualQuestions = 0;
+
+//questions
+import questions from "./questions.js";
+
+// substituição do quizz para perguntas
+function init() {
+    createQuestion(0);
+}
+
+// criar uma pergunta (limpar questão anterior e alterar texto pergunta)
+function createQuestion(i) {
+    const oldButtons = answersBox.querySelectorAll("button");
+    oldButtons.forEach(function (btn) {
+        btn.remove();
+    });
+
+    const questionText = question.querySelector("#question-text");
+    const questionNumber = question.querySelector("#question-number");
+
+    questionNumber.textContent = i + 1;
+    questionText.textContent = questions[0].question;
+}
+
+init();
